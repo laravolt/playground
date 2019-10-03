e5832b0894ac65059379e37e4021c901e5265d34
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-10-03 15:59:56 +0700

    Insert via query builder

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class ContactFormController extends Controller
 {
     public function create()
@@ -11,5 +13,31 @@ class ContactFormController extends Controller
 
     public function store(\App\Http\Requests\ContactForm\Store $request)
     {
+        // 1.a Insert menggunakan query builder (define fields satu per satu)
+        // DB::table('contact_forms')->insert([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'message' => $request->message,
+        // ]);
+
+        // 1.b Insert menggunakan query builder, memanfaatkan method only atau excep dari FormRequest
+        // untuk mem-filter fields apa saja yang ingin disimpan.
+        // DB::table('contact_forms')->insert($request->only(['name', 'email', 'message']));
+        // DB::table('contact_forms')->insert($request->except(['_token']));
+
+        // 1.c Sebenarnya bisa lebih singkat lagi dengan memanggil $request->all(),
+        // tapi akan menyebabkan error. Tahu kenapa?
+        // DB::table('contact_forms')->insert($request->all());
+
+        // 1.d Opsi lain adalah memanggil $request->validated(), dimana
+        // hanya field yg didefinisikan di FormRequest::rules() yang akan disimpan.
+        // WARNING: jika ada field yang tidak perlu divalidasi, tetap harus dituliskan di
+        // FormRequest tetapi cukup diset sebagai empty string atau empty array:
+        //        return [
+        //             'name' => ['required', new MinimumWords(2)],
+        //             'email' => ['required', 'email'],
+        //             'message' => [],
+        //         ];
+        // DB::table('contact_forms')->insert($request->validated());
     }
 }
